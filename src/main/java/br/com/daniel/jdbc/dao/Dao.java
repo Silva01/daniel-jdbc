cce584dcb844cf75b1c990d9e557054f4f4bacbd
Silva01 <silva20102@gmail.com>
2016-12-24 21:18:43 -0200

    Adicionado classe DAO com métodos de consulta Select

---
+++ src/main/java/br/com/daniel/jdbc/dao/Dao.java
@@ -0,0 +1,70 @@
+package br.com.daniel.jdbc.dao;
+
+import java.sql.ResultSet;
+import java.util.Date;
+
+import com.mysql.cj.jdbc.PreparedStatement;
+
+import br.com.daniel.jdbc.conexao.Conexao;
+import br.com.daniel.jdbc.exception.NaoConectadoDbException;
+import br.com.daniel.jdbc.util.Utilidades;
+
+public class Dao {
+
+	private Conexao conn;
+	private PreparedStatement stm;
+	
+	public Dao(final Conexao conn) {
+		this.conn = conn;
+	}
+	
+	public ResultSet select(final String query){
+		try {
+			this.stm = (PreparedStatement) conn.conectarMysql().prepareStatement(query);
+			ResultSet result = stm.executeQuery();
+			this.stm.close();
+			conn.conectarMysql().close();
+			return result;
+		} catch (NaoConectadoDbException e) {
+			new NaoConectadoDbException(e);
+			return null;
+		} catch (Exception e) {
+			new Exception(e);
+			return null;
+		}
+	}
+	
+	public ResultSet select(final String query, final Object...params){
+		try {
+			this.stm = (PreparedStatement) conn.conectarMysql().prepareStatement(query);
+			for (int i = 0; i < params.length; i++) {
+				if (params[i] instanceof String) {
+					this.stm.setString(i, Utilidades.convertTo(params[i]));
+				} else if (params[i] instanceof Integer) {
+					this.stm.setInt(i, Utilidades.convertTo(params[i]));
+				} else if (params[i] instanceof Boolean) {
+					this.stm.setBoolean(i, Utilidades.convertTo(params[i]));
+				} else if (params[i] instanceof Date) {
+					this.stm.setDate(i, Utilidades.convertTo(params[i]));
+				} else if (params[i] instanceof Double) {
+					this.stm.setDouble(i, Utilidades.convertTo(params[i]));
+				} else if (params[i] instanceof Float) {
+					this.stm.setFloat(i, Utilidades.convertTo(params[i]));
+				} else {
+					this.stm.setObject(i, params[i]);
+				}				
+			}
+			
+			ResultSet resul = this.stm.executeQuery();
+			
+			return resul;
+			 
+		} catch (NaoConectadoDbException e) {
+			new NaoConectadoDbException(e);
+			return null;
+		} catch (Exception e) {
+			new Exception(e);
+			return null;
+		}
+	}
+}
