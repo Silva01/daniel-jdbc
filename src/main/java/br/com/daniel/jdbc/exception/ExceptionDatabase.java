1e7db3fcb39dab5cce768e10d330d9e7e5fa6997
Daniel Silva <silva20102@gmail.com>
2017-03-11 12:51:50 -0300

    Adicionado JavaDoc as classes de Exception

--- src/main/java/br/com/daniel/jdbc/exception/ExceptionDatabase.java
+++ src/main/java/br/com/daniel/jdbc/exception/ExceptionDatabase.java
@@ -1,5 +1,12 @@
 package br.com.daniel.jdbc.exception;
 
+/**
+ * @author Daniel Silva
+ * @version 1.0.0
+ * 
+ * Classe que trata a exception de banco de dados, caso ocorra algum erro
+ *
+ */
 public class ExceptionDatabase extends Exception {
 
 	/**
@@ -8,14 +15,30 @@ public class ExceptionDatabase extends Exception {
 	private static final long serialVersionUID = -2159976118804974994L;
 
 
+	/**
+	 * Recebe uma exception para tratamento
+	 * 
+	 * @param e
+	 */
 	public ExceptionDatabase(final Exception e) {
 		super(e);
 	}
 	
+	/**
+	 * Recebe uma mensagem e uma exception para tratamento
+	 * 
+	 * @param message
+	 * @param e
+	 */
 	public ExceptionDatabase(final String message, final Exception e) {
 		super(message, e);
 	}
 	
+	/**
+	 * Recebe uma mensagem para tratamento da exception
+	 * 
+	 * @param message
+	 */
 	public ExceptionDatabase(final String message) {
 		super(message);
 	}
--- src/main/java/br/com/daniel/jdbc/exception/NaoEncontradoPropertiesException.java
+++ src/main/java/br/com/daniel/jdbc/exception/NaoEncontradoPropertiesException.java
@@ -1,24 +1,49 @@
 package br.com.daniel.jdbc.exception;
 
+/**
+ * @author Daniel Silva
+ * @version 1.0.0
+ * 
+ * Trata as exception causada por erros de leitura de arquivos de propriedades
+ *
+ */
 public class NaoEncontradoPropertiesException extends Exception {
 
 	/**
-	 * 
+	 * Serial ID
 	 */
 	private static final long serialVersionUID = -1480590513624460327L;
 
+	/**
+	 * Trata a exception utilizando o contrutor da superclasse Exception
+	 */
 	public NaoEncontradoPropertiesException() {
 		super();
 	}	
 	
+	/**
+	 * Recebe uma exception para tratamento
+	 * @param e
+	 */
 	public NaoEncontradoPropertiesException(final NaoEncontradoPropertiesException e) {
 		super(e);
 	}
 	
+	/**
+	 * Recebe uma mensagem e uma exception para tratamento
+	 * 
+	 * @param e
+	 * @param message
+	 */
 	public NaoEncontradoPropertiesException(final NaoEncontradoPropertiesException e, final String message) {
 		super(message, e);
 	}
 	
+	/**
+	 * Recebe uma mensagem para tratamento da exception
+	 * 
+	 * @param message
+	 */
 	public NaoEncontradoPropertiesException(final String message) {
 		super(message);
 	}
