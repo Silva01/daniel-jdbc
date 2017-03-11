a25460b69918aae9be9a121057a2d43c0deeccbe
Daniel Silva <silva20102@gmail.com>
2017-03-11 12:14:38 -0300

    Refatorado parametros do construtor

--- src/main/java/br/com/daniel/jdbc/conexao/Conexao.java
+++ src/main/java/br/com/daniel/jdbc/conexao/Conexao.java
@@ -1,12 +1,9 @@
 package br.com.daniel.jdbc.conexao;
 
-import java.io.FileInputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.util.Properties;
 
 import br.com.daniel.jdbc.exception.NaoConectadoDbException;
-import br.com.daniel.jdbc.exception.NaoEncontradoPropertiesException;
 
 /**
  * @author Daniel
@@ -14,7 +11,9 @@ import br.com.daniel.jdbc.exception.NaoEncontradoPropertiesException;
  * 
  * Classe faz a conexão com o banco de dados, ao realizar a conexão
  * retorna essa conexão pronta para que seja utilizada em por outras
- * classes de persistência
+ * classes de persistência.
+ * 
+ * Esta classe precisa que os parametros sejam informados 
  *
  */
 public class Conexao {
@@ -24,21 +23,19 @@ public class Conexao {
 	private String passDb;
 	
 	
+
 	/**
 	 * Contrutor no qual inicializa as principais variaveis que contém informações de 
 	 * conexão com o banco de dados
 	 * 
-	 * @throws NaoConectadoDbException
+	 * @param host
+	 * @param userDb
+	 * @param passDb
 	 */
-	public Conexao() throws NaoConectadoDbException {
-		try {
-			this.host = this.dados("db.host");
-			this.userDb = this.dados("db.user");
-			this.passDb = this.dados("db.pass");
-		} catch (Exception e) {
-			throw new NaoConectadoDbException((NaoConectadoDbException) e);
-		}
-		
+	public Conexao(final String host, final String userDb, final String passDb) {		
+		this.host = host;
+		this.userDb = userDb;
+		this.passDb = passDb;		
 	}
 	
 
@@ -74,24 +71,5 @@ public class Conexao {
 	
 	public Connection conectarSqlServer(){
 		return null;
-	}
-	
-	/**
-	 * @param campo do arquivo
-	 * @return dados do campo que se encontra no arquivo .properties
-	 * 
-	 * Ler o arquivo de propriedades e retorna os dados dos campos de acordo
-	 * com o campo passado por parametro
-	 * @throws NaoEncontradoPropertiesException 
-	 * @throws Exception 
-	 */
-	private String dados(final String campo) throws NaoEncontradoPropertiesException {
-		try (FileInputStream file = new FileInputStream("propriedades.properties")){			
-			Properties prop = new Properties();			
-			prop.load(file);
-			return prop.getProperty(campo);			
-		} catch (Exception e) {
-			throw new NaoEncontradoPropertiesException((NaoEncontradoPropertiesException) e, "Erro ao Ler arquivos de Propriedades");
-		}
-	}
+	}	
 }
